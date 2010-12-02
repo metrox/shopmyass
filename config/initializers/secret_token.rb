ee5bca167762e52d7fafc954f94c6b1f0a1f70a9
metrox <dawiddyderski@gmail.com>
2010-12-02 12:42:57 +0100

    new_shop_yo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleshop::Application.config.secret_token = 'cfd19bf19e7321a8cbc386661d07275ebb8f3318945003052a51812b85c9f9163c91784854ee1ffeaf95f20dfe6808dd6aff5cd6ecb092bb58ed7769640665fd'
